75962af8aec8ff348ea513fd28aed6fb40eabd94
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-03-04 14:53:06 +0100

    Show pizza.name and pizza.price

--- menu/views.py
+++ menu/views.py
@@ -6,6 +6,6 @@ from .models import Pizza
 
 def index(request):
     pizzas = Pizza.objects.all()
-    pizzas_names = [pizza.name for pizza in pizzas]
-    pizzas_names_str = ', '.join(pizzas_names)
-    return HttpResponse('Pizzas : ' + pizzas_names_str)
+    pizzas_names_and_price = [pizza.name + ' - ' + str(pizza.price) + ' $' for pizza in pizzas]
+    pizzas_names_and_price_str = ', '.join(pizzas_names_and_price)
+    return HttpResponse('Pizzas :  ' + pizzas_names_and_price_str)
