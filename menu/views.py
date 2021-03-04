355f4472ad75cf14ebfa8689da3b6e43eb4c1ddc
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-03-04 17:38:01 +0100

    create main page + url

--- menu/views.py
+++ menu/views.py
@@ -10,5 +10,5 @@ def index(request):
     pizzas_names_and_price = [pizza.name + ' - ' + str(pizza.price) + ' $' for pizza in pizzas]
     pizzas_names_and_price_str = ', '.join(pizzas_names_and_price)
     return HttpResponse('Pizzas : ' + pizzas_names_and_price_str)"""
-    pizza = Pizza.objects.all()
+    pizza = Pizza.objects.all().order_by('price')
     return render(request, 'menu/index.html', {'pizzas': pizza})
